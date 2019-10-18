1fab18e36b20838f62d74b4491b44b9362054d2d
Tasnuva <tasnuvah88@gmail.com>
2019-10-18 09:48:17 -0400

    final changes

--- src/screens/ResultsDetailScreen.js
+++ src/screens/ResultsDetailScreen.js
@@ -1,31 +1,54 @@
-import React, {useEffect} from 'react';
-import { View, Text, Button, Linking } from 'react-native';
+import React, {useEffect, useState} from 'react';
+import { View, Text, WebView, Button, Linking, StyleSheet } from 'react-native';
 import useResult from '../hooks/useResult';
 import { withNavigation } from 'react-navigation';
 
-const ResultsDetailScreen = () => {
+
+
+const ResultsDetailScreen = ( {navigation}) => {
   const pageid = navigation.getParam('pageid');
-  const [getInfo, res] = useResult();
- 
+  const [getInfo, res, errMessage] = useResult();
+
 useEffect(() => {
-  getInfo(pageid);
+ getInfo(pageid);
 }, []);
+  
+var resLink;
+ for (var p in res ) {
+    resLink = res[p].fullurl;
+   
+};
 
 
-  return (
-    <View>
-      <Text>{res.title}</Text>
-      {errMessage ? <Text>{errMessage}</Text> : null}
+   return (
+
+     <View style={styles.container} >
+        <WebView
+       style={styles.WebViewStyle}
+       source={{uri: `${resLink}`}} 
+       javaScriptEnabled={true}
+       domStorageEnabled={true}/>
+      
+       {errMessage ? <Text>{errMessage}</Text> : null}
       <Button
-        onPress={() => navigation.navigate('HomeScreen')}
-      >Go Back to Search Screen</Button>
+        title = 'HOME'
+        onPress={() => navigation.navigate('Home')}
+      ></Button>
       <Button
+       title='ANTHEM SITE'
         onPress={() => Linking.openURL('https://www.anthem.com')}>
-      Go to Anthem Site</Button>
+     </Button>
+    
     </View>
   );
 
-
+  
 };
+const styles = StyleSheet.create(
+  {
+    container: {
+      flex: 1,
+    },
+  });
 
 export default withNavigation(ResultsDetailScreen);
